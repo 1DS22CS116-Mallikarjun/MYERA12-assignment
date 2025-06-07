11a94b1884d6fe40cd4c3f0c24445bee8eb768f9
1DS22CS116-Mallikarjun <1ds22cs116@dsce.edu.in>
2025-06-07 11:35:18 +0530

    First commit

--- src/utils/stickers.js
+++ src/utils/stickers.js
@@ -1,8 +1,8 @@
 export const  stickersData = [
   { id: "smile", src: "https://img.icons8.com/color/48/smiling.png" },
-  { id: "angry", src: "https://img.icons8.com/ios-filled/50/angry.png" },
+  { id: "star", src: "https://img.icons8.com/ios-filled/50/star.png" },
   {
-    id: "star",
-    src: "https://img.icons8.com/external-sketchy-juicy-fish/60/external-emoji-emoji-sketchy-sketchy-juicy-fish-13.png",
+    id: "sad",
+    src: "https://img.icons8.com/color/48/sad.png",
   },
 ];
